2ccd6e13d0f67c46e8bec49d33ecb6f8bea4738a
Dylan Keyt <dylan.keyt@myer.com.au>
2020-06-26 09:23:38 +1000

    Use Catch logo and favicon and store them in the app, clean up Create React App logos and documentation

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
+import logo from '../../logo.svg'
 import DropDown from '../drop-down'
 import { SORT_METHODS } from '../../constants/sort'
 import { useProductState } from '../../context/product'
 import { useSortDispatch } from '../../context/sort'
-import { CATCH_LOGO_URL } from '../../constants/url'
 import { setSortMethod } from '../../actions/sort'
 
 const Header = () => {
@@ -16,7 +16,7 @@ const Header = () => {
 
 	return (
 		<header>
-			<img src={CATCH_LOGO_URL} alt='Catch' />
+			<img src={logo} alt='Catch' />
 			<div>Showing {query}</div>
 			<div>{total} results</div>
 			<div>
